84ee0a81ae728a08d23113fe4c4e464d062bf642
anushkaac <56971947+anushkaac@users.noreply.github.com>
2024-12-08 11:20:31 +0530

    Initial Commit

---
+++ IDEUIPanels/IDEUIPanelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDEUIPanelsApp.swift
+//  IDEUIPanels
+//
+//  Created by Anushka Chokshi on 2024-12-08.
+//
+
+import SwiftUI
+
+@main
+struct IDEUIPanelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
